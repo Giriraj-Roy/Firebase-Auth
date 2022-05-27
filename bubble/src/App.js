f6b64b937f12fa00adbc6b61834e4d0a579776be
Giriraj Roy <girirajroy2001@gmail.com>
2022-05-27 08:50:47 +0530

    Private Routes

--- bubble/src/App.js
+++ bubble/src/App.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Hell from './components/Home';
 import { AuthProvider } from './Auth';
+import PrivateRoute from './PrivateRoute';
+import Home from './components/Home';
+import Login from './Login';
+import SignUp from './SignUp';
 
 const App = () => {
   return (
@@ -11,7 +14,9 @@ const App = () => {
       <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route exact path='/' element={<Hell/>} />
+          <PrivateRoute exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/signup" element={<SignUp/>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
--- bubble/src/components/Home.js
+++ bubble/src/components/Home.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import app from "../Firebase";
 
-const Hell = () => {
+const Home = () => {
   return (
     <>
-        hello from home
+      <h1>Home</h1>
+      <button onClick={() => app.auth().signOut()}>Sign out</button>
     </>
-  )
-}
+  );
+};
 
-export default Hell
+export default Home;
